c4c9c2f454740867bd09e0ba0f10fca5918e092a
Miclefeng <miclefengzss@gmail.com>
2018-06-09 11:52:14 +0800

    edit

---
+++ pysql/__init__.py
@@ -0,0 +1,8 @@
+#=============================================================
+# File Name: __init__.py
+# Author: miclefeng
+# mail: miclefengzss@163.com
+# Created Time: Sat 09 Jun 2018 11:46:35 AM CST
+#=============================================================
+# coding:utf8
+
--- pysql/mysql/test_con_mysql.py
+++ pysql/mysql/test_con_mysql.py
@@ -1,8 +1,12 @@
 #=============================================================
-# File Name: news_model.py
+# File Name: test_con_mysql.py
 # Author: miclefeng
 # mail: miclefengzss@163.com
-# Created Time: Sat 09 Jun 2018 10:20:52 AM CST
+# Created Time: Sat 09 Jun 2018 11:38:52 AM CST
 #=============================================================
 # coding:utf8
+import pymysql
+from pysql.db_conf import *
 
+
+print(HOST)
