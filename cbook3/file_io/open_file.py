321e971bf30a2e091cf17fc0db1f7642c9908285
Miclefeng <miclefengzss@gmail.com>
2018-06-14 16:53:06 +0800

    edit

--- cbook3/file_io/open_file.py
+++ cbook3/file_io/open_file.py
@@ -5,6 +5,9 @@
 # Created Time: Tue 12 Jun 2018 06:23:40 PM CST
 #=============================================================
 # coding:utf8
+import array
+from functools import partial
+
 
 with open('somefile.txt', 'rt') as f:
     # data = f.read()
@@ -21,3 +24,20 @@ with open('somefile.txt', 'wt') as f:
     print('python', file=f)
     print('flask', file=f)
     print('scrapy', file=f)
+
+nums = array.array('i', [1, 2, 3, 4])
+#with open('data.bin', 'wb') as f:
+#    f.write(nums)
+
+with open('data.bin', 'wb') as f:
+    f.write('this is a test miclefengzss brightdh admin python flask scrapy high skills sql'.encode('utf-8'))
+
+RECORD_SIZE = 32
+with open('data.bin', 'rb') as f:
+    # iter()如果你给它传递一个可调用对象和一个标记值，它会创建一个迭代器。
+    # 这个迭代器会一直调用传入的可调用对象直到它返回标记值为止，这时候迭代终止
+    # unctools.partial 用来创建一个每次被调用时从文件中读取固定数目字节的可调用对象。 标记值 b''
+    # 就是当到达文件结尾时的返回值
+    records = iter(partial(f.read, RECORD_SIZE), b'')
+    for r in records:
+        print(r)
