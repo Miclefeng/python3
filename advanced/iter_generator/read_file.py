cf694b97511fc22b5712220bf6a3fc32c17bdbac
ZSShang <1280054628@qq.com>
2018-07-08 11:18:23 +0800

    edit

--- advanced/iter_generator/read_file.py
+++ advanced/iter_generator/read_file.py
@@ -10,11 +10,16 @@ def myreadlines(f, newline):
     buf = ''
     while True:
         while newline in buf:
+            # 获取分隔符所在的位置
             pos = buf.index(newline)
+            # 返回分隔符第一段数据
             yield buf[:pos]
+            # 剔除上一次分割出的第一段数据
             buf = buf[pos + len(newline):]
+        # 每次读取4096个字节
         chunk = f.read(4096)
 
+        # 判断是否到文件末尾
         if not chunk:
             yield buf
             break
