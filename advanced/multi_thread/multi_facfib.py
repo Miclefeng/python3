93cf3199b754a7b04750708a0e1d731c34e1075e
ZSShang <1280054628@qq.com>
2018-07-10 22:14:31 +0800

    edit

---
+++ advanced/multi_thread/multi_facfib.py
@@ -0,0 +1,58 @@
+#=============================================================
+# File Name: user.py
+# Author: miclefeng
+# mail: miclefengzss@163.com
+# Created Time: 2018/7/10 21:59
+#=============================================================
+# coding:utf8
+import sys
+sys.path.extend(['python3'])
+from advanced.multi_thread.threading_thread2 import MyThread
+from time import ctime, sleep
+
+
+def fib(x):
+    sleep(0.005)
+    if x < 2: return 1
+    return fib(x - 2) + fib(x - 1)
+
+def fac(x):
+    sleep(0.1)
+    if x < 2: return 1
+    return x * fac(x - 1)
+
+def sum(x):
+    sleep(0.1)
+    if x < 2: return 1
+    return x + sum(x - 1)
+
+funcs = [fib, fac, sum]
+n = 12
+
+def main():
+    nfuncs = range(len(funcs))
+
+    print('**** SINGLE THREAD')
+    for i in nfuncs:
+        print('starting ', funcs[i].__name__,  ' at: ', ctime())
+        print(funcs[i](n))
+        print(funcs[i].__name__, ' finished at: ', ctime())
+
+    print('\n**** MULTIPLE THREADS')
+    threads = []
+    for i in nfuncs:
+        t = MyThread(funcs[i], (n, ), funcs[i].__name__)
+        threads.append(t)
+
+    for i in nfuncs:
+        threads[i].start()
+
+    for i in nfuncs:
+        threads[i].join()
+        print(threads[i].getResult())
+    print('all DONE')
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ advanced/multi_thread/threading_thread2.py
@@ -0,0 +1,53 @@
+#=============================================================
+# File Name: user.py
+# Author: miclefeng
+# mail: miclefengzss@163.com
+# Created Time: 2018/7/10 21:12
+#=============================================================
+# coding:utf8
+import threading
+from time import ctime, sleep
+
+
+loops = [4, 2]
+
+class MyThread(threading.Thread):
+    def __init__(self, func, args, name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def getResult(self):
+        return self.res
+
+    def run(self):
+        print('starting ', self.name, ' at: ', ctime())
+        self.res = self.func(*self.args)
+        print(self.name, ' finished at: ', ctime())
+
+def loop(nloop, nsec):
+    print('start loop ', nloop, ' at: ', ctime())
+    sleep(nsec)
+    print('loop', nloop, ' done at: ', ctime())
+
+def main():
+    print('starting at: ', ctime())
+    threads = []
+    nloops = range(len(loops))
+
+    for i in nloops:
+        t = MyThread(loop, (i, loops[i]), loop.__name__)
+        threads.append(t)
+
+    for i in nloops:
+        threads[i].start()
+
+    for i in nloops:
+        # 等待所有线程执行完成
+        threads[i].join()
+
+    print('all DONE at: ', ctime())
+
+if __name__ == '__main__':
+    main()
