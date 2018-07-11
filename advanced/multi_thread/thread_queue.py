c3150e9c2238fd0a9b95739756f0511da3200828
zhangshangshang <zhangshangshang@modian.com>
2018-07-11 11:55:30 +0800

    edit

--- advanced/multi_thread/thread_queue.py
+++ advanced/multi_thread/thread_queue.py
@@ -11,34 +11,39 @@ from queue import Queue
 
 def get_detail_html(queue):
     # 抓取文章详情页
-    while True:
-        url = queue.get()
-        print('get detail html started')
-        time.sleep(2)
-        print(url)
-        print('get detail html end')
+    url = queue.get(1)
+    print('get detail html started')
+    time.sleep(2)
+    print(url)
+    print('get detail html end')
 
 
 def get_detail_url(queue):
     # 爬取文章列表页
-    while True:
-        print('get detail url started')
-        time.sleep(3)
-        for i in range(21):
-            queue.put('http://miclefeng.com/{id}'.format(id=i))
-        print('get detail url end')
+    print('get detail url started')
+    time.sleep(3)
+    for i in range(21):
+        queue.put('http://miclefeng.com/{id}'.format(id=i), 1)
+    print('get detail url end')
 
 if __name__ == '__main__':
     detail_url_queue = Queue(maxsize=100)
     url_thread = threading.Thread(target=get_detail_url, args=(detail_url_queue, ))
-    # url_thread.start()
-    for i in range(3):
-        html_thread = threading.Thread(target=get_detail_html, args=(detail_url_queue, ))
-        html_thread.start()
 
     start_time = time.time()
-    detail_url_queue.task_done()
-    detail_url_queue.join()
+    threads = []
+    for i in range(21):
+        t = threading.Thread(target=get_detail_html, args=(detail_url_queue, ))
+        threads.append(t)
+
+    url_thread.start()
+    for i in range(21):
+        threads[i].start()
+
+    url_thread.join()
+    for i in range(21):
+        threads[i].join()
+
 
     print('last time: {}'.format(time.time() - start_time))
 
