c31efe75f5395d89d1661f67ef71219bba96de02
ZSShang <1280054628@qq.com>
2018-07-30 01:13:29 +0800

    edit

--- advanced/multi_process/fork_process.py
+++ advanced/multi_process/fork_process.py
@@ -16,7 +16,7 @@ def main():
     if pid == 0:
         print('子进程：{}，父进程：{}。'.format(os.getpid(), os.getppid()))
     else:
-        print('我是父进程：{}'.format(pid))
+        print('我是父进程：{}'.format(os.getpid()))
     sleep(2)
 
 if __name__ == '__main__':
