f63089c35e28d6c3659849b207ddf47ce3668665
ZSShang <1280054628@qq.com>
2018-07-30 00:57:17 +0800

    edit

---
+++ advanced/multi_process/fork_process.py
@@ -0,0 +1,23 @@
+#=============================================================
+# File Name: user.py
+# Author: miclefeng
+# mail: miclefengzss@163.com
+# Created Time: 2018/7/30 0:54
+#=============================================================
+# coding:utf8
+import os
+from time import ctime, sleep
+
+
+def main():
+    pid = os.fork()
+    print('miclefeng')
+
+    if pid == 0:
+        print('子进程：{}，父进程：{}。'.format(os.getpid(), os.getppid()))
+    else:
+        print('我是父进程：{}'.format(pid))
+    sleep(2)
+
+if __name__ == '__main__':
+    main()
