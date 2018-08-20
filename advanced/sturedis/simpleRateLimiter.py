96645c5f46d96f8354be12e68a59269e259def99
zhangshangshang <zhangshangshang@modian.com>
2018-08-20 17:51:27 +0800

    edit

--- advanced/sturedis/simpleRateLimiter.py
+++ advanced/sturedis/simpleRateLimiter.py
@@ -11,9 +11,18 @@ def is_action_allowed(user_id, action_key, period, max_count):
 
     with client.pipeline() as pipe:
         pipe.set(key + '_lock', True, nx=True, ex=5)
+
+        # 保证value的值的唯一性
         pipe.zadd(key, now_ts, now_ts)
+
+        # 保留窗口之内的数据，删除超过限流时间的数据
+        # 0_15_30_45_60_75_90, 30_45_60_75_90 删除窗口之外的数据，保留限流时间段内的数据
         pipe.zremrangebyscore(key, 0, now_ts - period * 10000)
+
+        # 查询当前窗口的数据大小
         pipe.zcard(key)
+
+        # 设置过期时间，避免冷数据占用内存
         pipe.expire(key, period + 1)
         pipe.delete(key + '_lock')
         _, _, _, current_count, _, _ = pipe.execute()
@@ -21,6 +30,7 @@ def is_action_allowed(user_id, action_key, period, max_count):
     return current_count <= max_count
 
 if __name__ == '__main__':
-    for i in range(20):
+    for i in range(30):
+        time.sleep(1)
         res = is_action_allowed('miclefeng', 'reply', 60, 10)
         print(res)
