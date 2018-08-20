93ba921811d565ee290a1a15fedcd3b61e5e919b
Miclefeng <miclefengzss@gmail.com>
2018-08-20 17:34:44 +0800

    edit

--- advanced/sturedis/simpleRateLimiter.py
+++ advanced/sturedis/simpleRateLimiter.py
@@ -7,7 +7,20 @@ client = redis.StrictRedis()
 
 def is_action_allowed(user_id, action_key, period, max_count):
     key = 'hist:{:s}:{:s}'.format(user_id, action_key)
-    now_ts = int(time.time() * 1000)
+    now_ts = int(time.time() * 10000)
 
     with client.pipeline() as pipe:
-        pipe.zadd()
+        pipe.set(key + '_lock', True, nx=True, ex=5)
+        pipe.zadd(key, now_ts, now_ts)
+        pipe.zremrangebyscore(key, 0, now_ts - period * 10000)
+        pipe.zcard(key)
+        pipe.expire(key, period + 1)
+        pipe.delete(key + '_lock')
+        _, _, _, current_count, _, _ = pipe.execute()
+        print(current_count, now_ts)
+    return current_count <= max_count
+
+if __name__ == '__main__':
+    for i in range(20):
+        res = is_action_allowed('miclefeng', 'reply', 60, 10)
+        print(res)
